8e6f2612993642173e5d322442bb96cb3666a95c
Hoang Minh <minhbeo135@gmail.com>
2019-01-24 19:35:14 +0700

    fix spelling mistake

--- main-controller.js
+++ main-controller.js
@@ -1,7 +1,7 @@
 function fixStyleBanner () {
-    if (screen.width < 1300) {
+    if (window.width < 1300) {
         document.getElementById("banner-1").style.backgroundPosition = "left";
     } else {
         document.getElementById("banner-1").style.backgroundPosition = "center";
     }
-};
+}
