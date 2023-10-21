2ac2914f32318584dd906a993b49f67b9b081271
Hoang Tien Minh <htminh.1001@gmail.com>
2023-10-22 02:43:48 +0700

    feat: add portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      aspectRatio: {
+        "16/9": "16 / 9",
+      },
+      inset: {
+        "25%": "25%",
+        "55%": "55%",
+        "85%": "85%",
+        "115%": "115%",
+        "145%": "145%",
+        "175%": "175%",
+      },
+      transitionDelay: {
+        400: "400ms",
+        500: "500ms",
+        600: "600ms",
+      },
+    },
+  },
+  plugins: [],
+};
